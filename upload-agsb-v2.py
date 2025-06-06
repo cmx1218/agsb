0acd7c2518deccc2fadece39e9aef932b0c95ac2
cmx1218 <168510364+cmx1218@users.noreply.github.com>
2025-06-06 15:37:28 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
+USER_NAME = "cy0606"         # 用户名
+UUID = "edeceb95-72fe-46b4-957d-fb0d3698456c"                     # UUID，留空则自动生成
+PORT = 39999                 # Vmess端口，留空或0则自动生成
 DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+CF_TOKEN = "eyJhIjoiMjk1M2U5NGZkZDkxNDlhNWQ4NWQ4OGI5ODY3MjYyMjUiLCJ0IjoiZTU2ZjViOTgtMzAwNS00M2EzLTk3YzYtOWM3M2YxYWM2OThhIiwicyI6Ik5URmlNemszWkRjdFpEa3dNaTAwTXpSakxUaG1OakF0WkdWa05EVm1PRE13TkRjMSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
